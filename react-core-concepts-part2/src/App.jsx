b9485ee86bd451fad59f3ea5eb52a16883eff591
erab2r <shkera213@gmail.com>
2026-01-05 01:45:47 +0600

    1

--- react-core-concepts-part2/src/App.jsx
+++ react-core-concepts-part2/src/App.jsx
@@ -2,10 +2,18 @@
 import './App.css'
 
 function App() {
+  function handleClick() {
+    alert('Habibi!')
+  }
+  const handleClick2 = () => {
+    alert('ERA 2!')
+  }
 
   return (
     <>
       <h1>Vite + React</h1>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Me</button>
     </>
   )
 }
