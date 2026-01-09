1b49f68164c194a9026535b13382582587f1be42
erab2r <shkera213@gmail.com>
2026-01-10 00:10:20 +0600

    1

--- react-core-concepts-part2/src/App.jsx
+++ react-core-concepts-part2/src/App.jsx
@@ -1,4 +1,4 @@
-
+import Counter from './counter';
 import './App.css'
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Counter></Counter>
       <button onClick={handleClick}>Click Me</button>
       
       <button onClick={handleClick2}>Click Me</button>
